f1d629d3c998f6f4fb6e4299962f2916ca703c76
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-04-23 18:53:55 +0100

    Initial commit of code and tests.

---
+++ tests/conftest.py
@@ -0,0 +1,66 @@
+# stdlib
+from copy import deepcopy
+
+# 3rd party
+import pytest
+from domdf_python_tools.paths import PathPlus
+from pyms.BillerBiemann import BillerBiemann  # type: ignore
+from pyms.GCMS.IO.JCAMP import JCAMP_reader  # type: ignore
+from pyms.IntensityMatrix import build_intensity_matrix_i  # type: ignore
+from pyms.Noise.SavitzkyGolay import savitzky_golay  # type: ignore
+from pyms.TopHat import tophat  # type: ignore
+
+pytest_plugins = ("coincidence", )
+
+
+@pytest.fixture(scope="session")
+def pyms_datadir():
+	return PathPlus(__file__).parent / "data"
+
+
+@pytest.fixture(scope="session")
+def data(pyms_datadir):
+	return JCAMP_reader(pyms_datadir / "ELEY_1_SUBTRACT.JDX")
+
+
+@pytest.fixture(scope="session")
+def tic(data):
+	# get the TIC
+	return deepcopy(data.tic)
+
+
+@pytest.fixture(scope="session")
+def im_i(data):
+	# build an intensity matrix object from the data
+	return build_intensity_matrix_i(data)
+
+
+@pytest.fixture()
+def ms(im_i):
+	return deepcopy(im_i.get_ms_at_index(0))
+
+
+@pytest.fixture(scope="session")  # noqa: PT005
+def _peak_list(im_i):
+	im_i = deepcopy(im_i)
+
+	# Intensity matrix size (scans, masses)
+	n_scan, n_mz = im_i.size
+
+	# noise filter and baseline correct
+	for ii in range(n_mz):
+		ic = im_i.get_ic_at_index(ii)
+		ic_smooth = savitzky_golay(ic)
+		ic_bc = tophat(ic_smooth, struct="1.5m")
+		im_i.set_ic_at_index(ii, ic_bc)
+
+	# Use Biller and Biemann technique to find apexing ions at a scan
+	# default is maxima over three scans and not to combine with any neighbouring
+	# scan.
+	peak_list = BillerBiemann(im_i, points=9, scans=2)
+	return peak_list
+
+
+@pytest.fixture()
+def peak_list(_peak_list):
+	return deepcopy(_peak_list)
---
+++ tests/test_plot.py
@@ -0,0 +1,346 @@
+# stdlib
+import os
+
+# 3rd party
+import pytest
+from coincidence.regressions import AdvancedFileRegressionFixture
+from domdf_python_tools.paths import PathPlus
+from matplotlib import axes, figure  # type: ignore
+from matplotlib import pyplot as plt
+from matplotlib.backend_bases import MouseEvent  # type: ignore
+from matplotlib.cbook import CallbackRegistry  # type: ignore
+
+# this package
+from pymassspec_plot import ClickEventHandler, plot_head2tail, plot_ic, plot_mass_spec, plot_peaks
+from tests.constants import test_dict, test_numbers, test_sequences, test_string
+
+baseline_dir = str(PathPlus(__file__).parent / "baseline")
+assert os.path.exists(baseline_dir)
+
+image_hashes = str(PathPlus(__file__).parent / "image_hashes.json")
+
+check_images = pytest.mark.mpl_image_compare(
+		baseline_dir=baseline_dir,
+		savefig_kwargs={"dpi": 600},
+		hash_library=image_hashes,
+		style="default",
+		)
+
+
+@check_images
+def test_plot_ic(im_i):
+	fig, ax = plt.subplots()
+
+	# Plotting IC with various Line2D options
+	plot_ic(ax, im_i.get_ic_at_index(5))
+
+	return fig
+
+
+@check_images
+def test_plot_ic_minutes(im_i):
+	fig, ax = plt.subplots()
+
+	# Plotting IC with various Line2D options
+	plot_ic(ax, im_i.get_ic_at_index(5), minutes=True)
+
+	return fig
+
+
+@check_images
+def test_plot_ic_label(im_i):
+	fig, ax = plt.subplots()
+
+	plot_ic(ax, im_i.get_ic_at_index(5), label="IC @ Index 5")
+	ax.legend()
+
+	return fig
+
+
+@check_images
+def test_plot_ic_alpha(im_i):
+	fig, ax = plt.subplots()
+
+	plot_ic(ax, im_i.get_ic_at_index(5), alpha=0.5)
+
+	return fig
+
+
+@check_images
+def test_plot_ic_linewidth(im_i):
+	fig, ax = plt.subplots()
+
+	plot_ic(ax, im_i.get_ic_at_index(5), linewidth=2)
+
+	return fig
+
+
+@check_images
+def test_plot_ic_linestyle(im_i):
+	fig, ax = plt.subplots()
+
+	plot_ic(ax, im_i.get_ic_at_index(5), linestyle="--")
+
+	return fig
+
+
+@check_images
+def test_plot_ic_multiple(im_i):
+	fig, ax = plt.subplots()
+
+	plot_ic(ax, im_i.get_ic_at_index(5), label="IC @ Index 5")
+	plot_ic(ax, im_i.get_ic_at_index(10), label="IC @ Index 10")
+	plot_ic(ax, im_i.get_ic_at_index(20), label="IC @ Index 20")
+	plot_ic(ax, im_i.get_ic_at_index(40), label="IC @ Index 40")
+	plot_ic(ax, im_i.get_ic_at_index(80), label="IC @ Index 80")
+	plot_ic(ax, im_i.get_ic_at_index(160), label="IC @ Index 160")
+
+	ax.legend()
+
+	return fig
+
+
+@check_images
+def test_plot_ic_title(im_i):
+	fig, ax = plt.subplots()
+
+	plot_ic(ax, im_i.get_ic_at_index(5))
+	ax.set_title("Test IC Plot")
+
+	return fig
+
+
+def test_plot_ic_errors(im_i, data, ms):
+	fig, ax = plt.subplots()
+
+	for obj in [*test_sequences, test_string, *test_numbers, test_dict, im_i, data, ms]:
+		with pytest.raises(TypeError, match="'ic' must be an IonChromatogram"):
+			plot_ic(ax, obj)
+
+
+# Plotting tic with various Line2D options
+@check_images
+def test_plot_tic(tic):
+	fig, ax = plt.subplots()
+
+	plot_ic(ax, tic)
+
+	return fig
+
+
+@check_images
+def test_plot_tic_label(tic):
+	fig, ax = plt.subplots()
+
+	plot_ic(ax, tic, label="IC @ Index 5")
+
+	ax.legend()
+
+	return fig
+
+
+@check_images
+def test_plot_tic_alpha(tic):
+	fig, ax = plt.subplots()
+
+	plot_ic(ax, tic, alpha=0.5)
+
+	return fig
+
+
+@check_images
+def test_plot_tic_linewidth(tic):
+	fig, ax = plt.subplots()
+
+	plot_ic(ax, tic, linewidth=2)
+
+	return fig
+
+
+@check_images
+def test_plot_tic_linestyle(tic):
+	fig, ax = plt.subplots()
+
+	plot_ic(ax, tic, linestyle="--")
+
+	return fig
+
+
+def test_plot_tic_errors(im_i, data, ms):
+	fig, ax = plt.subplots()
+
+	for obj in [
+			*test_sequences,
+			*test_numbers,
+			test_string,
+			test_dict,
+			im_i,
+			data,
+			ms,
+			]:
+		with pytest.raises(TypeError, match="'ic' must be an IonChromatogram"):
+			plot_ic(ax, obj)
+
+
+@check_images
+def test_plot_tic_title(tic):
+	fig, ax = plt.subplots()
+
+	plot_ic(ax, tic)
+
+	ax.set_title("Test TIC Plot")
+
+	return fig
+
+
+# Plotting mass spec with various Line2D options
+@check_images
+def test_plot_mass_spec(im_i):
+	fig, ax = plt.subplots()
+
+	plot_mass_spec(ax, im_i.get_ms_at_index(50))
+
+	return fig
+
+
+@check_images
+def test_plot_mass_spec_alpha(im_i):
+	fig, ax = plt.subplots()
+
+	plot_mass_spec(ax, im_i.get_ms_at_index(50), alpha=0.5)
+
+	return fig
+
+
+@check_images
+def test_plot_mass_spec_width(im_i):
+	fig, ax = plt.subplots()
+
+	plot_mass_spec(ax, im_i.get_ms_at_index(50), width=1)
+
+	return fig
+
+
+@check_images
+def test_plot_mass_spec_linestyle(im_i):
+	fig, ax = plt.subplots()
+
+	plot_mass_spec(ax, im_i.get_ms_at_index(50), linestyle="--")
+
+	return fig
+
+
+def test_plot_mass_spec_errors(im_i, data, tic):
+	fig, ax = plt.subplots()
+
+	for obj in [
+			*test_sequences,
+			test_string,
+			*test_numbers,
+			test_dict,
+			im_i,
+			im_i.get_ic_at_index(0),
+			data,
+			tic,
+			]:
+		with pytest.raises(TypeError, match="'mass_spec' must be a MassSpectrum"):
+			plot_mass_spec(ax, obj)
+
+
+@check_images
+def test_plot_mass_spec_title(im_i):
+	fig, ax = plt.subplots()
+
+	plot_mass_spec(ax, im_i.get_ms_at_index(50))
+
+	ax.set_title(f"Mass spec for peak at time {im_i.get_time_at_index(50):5.2f}")
+
+	return fig
+
+
+# TODO: plot_peaks
+
+
+@check_images
+def test_plot_head2tail(im_i):
+	fig, ax = plt.subplots()
+
+	plot_head2tail(ax, im_i.get_ms_at_index(50), im_i.get_ms_at_index(150))
+
+	return fig
+
+
+def test_plot_head2tail_errors(im_i, data):
+	fig, ax = plt.subplots()
+
+	for obj in [*test_sequences, test_string, *test_numbers, test_dict, im_i, data]:
+		with pytest.raises(TypeError, match="'top_mass_spec' must be a MassSpectrum"):
+			plot_head2tail(ax, obj, im_i.get_ms_at_index(150))
+
+		with pytest.raises(TypeError, match="'bottom_mass_spec' must be a MassSpectrum"):
+			plot_head2tail(ax, im_i.get_ms_at_index(50), obj)
+
+	for obj in [*test_sequences, test_string, *test_numbers, im_i.get_ms_at_index(250), im_i, data]:
+		with pytest.raises(
+				TypeError,
+				match="'top_spec_kwargs' must be a mapping of keyword arguments for the top mass spectrum."
+				):
+			plot_head2tail(ax, im_i.get_ms_at_index(50), im_i.get_ms_at_index(150), top_spec_kwargs=obj)
+
+		with pytest.raises(
+				TypeError,
+				match="'bottom_spec_kwargs' must be a mapping of keyword arguments for the bottom mass spectrum."
+				):
+			plot_head2tail(ax, im_i.get_ms_at_index(50), im_i.get_ms_at_index(150), bottom_spec_kwargs=obj)
+
+
+def test_plot_peaks(peak_list, im_i, data, ms):
+	fig, ax = plt.subplots()
+
+	for obj in [*test_sequences, test_string, *test_numbers, test_dict, im_i, data, ms]:
+		with pytest.raises(TypeError, match="'peak_list' must be a list of Peak objects"):
+			plot_peaks(ax, obj)
+
+
+@check_images
+def test_plot_peaks_errors(peak_list):
+	fig, ax = plt.subplots()
+
+	plot_peaks(ax, peak_list)
+
+	return fig
+
+
+@check_images
+def test_plot_peaks_with_tic(peak_list, tic):
+	fig, ax = plt.subplots()
+
+	plot_ic(ax, tic)
+	plot_peaks(ax, peak_list)
+
+	return fig
+
+
+def test_click_event_handler(
+		peak_list,
+		capsys,
+		advanced_file_regression: AdvancedFileRegressionFixture,
+		):
+	fig, ax = plt.subplots()
+
+	# Setup up the ClickEventHandler
+	handler = ClickEventHandler(peak_list, fig, ax)
+
+	assert handler.fig is fig
+	assert handler.ax is ax
+	assert handler.cid is not None
+
+	callbacks: CallbackRegistry = fig.canvas.callbacks
+	assert "button_press_event" in callbacks.callbacks
+
+	event = MouseEvent("button_press_event", fig.canvas, peak_list[0].rt, 100, button=1)
+	event.xdata = peak_list[0].rt  # FIXME
+
+	callbacks.process("button_press_event", event)
+
+	advanced_file_regression.check(capsys.readouterr().out)
